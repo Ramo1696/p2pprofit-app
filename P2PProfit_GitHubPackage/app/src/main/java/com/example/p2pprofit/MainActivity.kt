b39df81057f5c2a6b4cf2a881c6c4d150e31a768
Ramo1696 <ramopirveli16@gmail.com>
2025-05-07 18:48:52 +0400

    Add files via upload

---
+++ P2PProfit_GitHubPackage/app/src/main/java/com/example/p2pprofit/MainActivity.kt
@@ -0,0 +1,18 @@
+
+package com.example.p2pprofit
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("file:///android_asset/index.html")
+        setContentView(webView)
+    }
+}
